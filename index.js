93aff5f242267a6ad7fb91e2d7db18872fc1fe70
Michał Salabura <micheu334@gmail.com>
2023-05-17 21:23:12 +0200

    Change colors on hover and select

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ let selected = 0;
 rates.forEach(rate => {
     rate.addEventListener("click",e => {
         rates.forEach(rate => {
-            rate.classList.remove('previous')
             rate.style.backgroundColor = '';
             rate.style.color = '';
         })
         rate.style.backgroundColor = 'hsl(25, 97%, 53%)';
         rate.style.color = "hsl(0, 0%, 100%)";
         selected = rate.id;
-        if(Number(rate.id) != 1) {
-            const previous = document.getElementById(Number(rate.id) - 1);
-            previous.classList.add('previous');
-        }
     })
 });
 
